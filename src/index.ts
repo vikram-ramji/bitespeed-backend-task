25784c0aa7b42bd459d59b3fceb7be59c335deec
Vikram Ramji Iyer <vikramramji24@gmail.com>
2025-07-11 15:01:20 +0000

    feat: add zod schema for input validation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import express, { Request, Response } from "express"
+import zod from "zod"
+import { prisma } from "./db/client"
 
 const app = express()
 
-app.use(express.json)
+app.use(express.json())
 
-app.post("/identify", (req: Request, res: Response) => {
-    // Identity Reconciliation logic
+const ContactInput = zod.object({
+    email: zod.email().optional(),
+    phoneNumber: zod.string().optional()
+}).refine(
+    data => data.email || data.phoneNumber,
+    { message: "At least one of email or phoneNumber is required" }
+)
+
+app.post("/identify", async (req: Request, res: Response) => {
+    // Validate request input/payload
+    const parsed = ContactInput.safeParse(req.body)
+
+    if (!parsed.success) {
+        return res.status(400).json({
+            message: "Invalid Input"
+        })
+    }
+
+    // Get the validated inputData fully typed
+    const inputData = parsed.data
 })
 
 app.listen(3000, () => {
